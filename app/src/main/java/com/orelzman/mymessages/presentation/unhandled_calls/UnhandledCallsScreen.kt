ad5ff88b5e4eef6cb2d8917cf1858e4e6933c38f
orelsgit <orelsmail@gmail.com>
2022-06-18 18:35:21 +0300

    - Unhandled calls are now presented in a list

--- app/src/main/java/com/orelzman/mymessages/presentation/unhandled_calls/UnhandledCallsScreen.kt
+++ app/src/main/java/com/orelzman/mymessages/presentation/unhandled_calls/UnhandledCallsScreen.kt
@@ -1,10 +1,12 @@
 package com.orelzman.mymessages.presentation.unhandled_calls
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.google.accompanist.flowlayout.FlowRow
 import com.orelzman.mymessages.presentation.unhandled_calls.components.UnhandledCallRow
 import com.orelzman.mymessages.util.extension.DefaultDestinationNavigator
 import com.ramcosta.composedestinations.annotation.Destination
@@ -16,16 +18,22 @@ fun UnhandledCallsScreen(
     navigator: DestinationsNavigator,
     viewModel: UnhandledCallsViewModel = hiltViewModel()
 ) {
-
     val state = viewModel.state
-
     viewModel.initCalls(LocalContext.current)
-
-    FlowRow {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
         state.callsToHandle.forEach {
-            UnhandledCallRow(
-                phoneCall = it.phoneCall
-            )
+            Box(
+                modifier = Modifier
+                    .height(60.dp)
+                    .padding(),
+            ) {
+                UnhandledCallRow(
+                    phoneCall = it.phoneCall
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/orelzman/mymessages/presentation/unhandled_calls/components/UnhandledCallRow.kt
+++ app/src/main/java/com/orelzman/mymessages/presentation/unhandled_calls/components/UnhandledCallRow.kt
@@ -1,15 +1,17 @@
 package com.orelzman.mymessages.presentation.unhandled_calls.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Divider
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.orelzman.mymessages.R
 import com.orelzman.mymessages.data.dto.PhoneCall
 
@@ -24,21 +26,28 @@ fun UnhandledCallRow(
     Row(
         modifier = modifier
             .fillMaxSize()
+            .background(Color.White),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
             painter = painterResource(id = R.drawable.ic_baseline_remove_circle_24),
             contentDescription = "Remove unhandled call",
+            modifier = Modifier
+                .size(30.dp)
+                .clickable { onDelete(phoneCall) },
             tint = MaterialTheme.colorScheme.error
         )
-        Divider()
-        Column {
+        Spacer(modifier = Modifier.weight(1f))
+        Column(modifier = Modifier.clickable { onClick(phoneCall) }) {
             Text(text = phoneCall.name)
             Text("14:12 • אתמול")
         }
-        Divider()
         Icon(
             painter = painterResource(id = R.drawable.ic_call_24),
             contentDescription = "Remove unhandled call",
+            modifier = Modifier
+                .size(30.dp)
+                .clickable { onCall(phoneCall) },
             tint = MaterialTheme.colorScheme.surface
         )
     }
