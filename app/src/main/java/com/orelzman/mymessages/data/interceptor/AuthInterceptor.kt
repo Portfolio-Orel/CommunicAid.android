71ec89c57c6bd0552c49d5fbacc2dd14b20a1d01
orelsgit <orelsmail@gmail.com>
2022-07-21 14:15:49 +0300

    - For commit

--- app/src/main/java/com/orelzman/mymessages/data/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/orelzman/mymessages/data/interceptor/AuthInterceptor.kt
@@ -7,7 +7,6 @@ import okhttp3.Response
 class AuthInterceptor (
     private val authInteractor: AuthInteractor
 ) : Interceptor {
-
     private val lock = Any()
     override fun intercept(chain: Interceptor.Chain): Response =
         takeIf { synchronized(lock) { authInteractor.getUser()?.token != "" } }
