ac7c8b5de6040f5ab59cc72ddda9fe07133fc250
orelsgit <orelsmail@gmail.com>
2022-10-15 13:52:57 +0300

    - Better logging to http requests

--- app/src/main/java/com/orelzman/mymessages/data/interceptor/LogInterceptor.kt
+++ app/src/main/java/com/orelzman/mymessages/data/interceptor/LogInterceptor.kt
@@ -1,5 +1,6 @@
 package com.orelzman.mymessages.data.interceptor
 
+import com.google.gson.Gson
 import com.orelzman.mymessages.domain.util.extension.Logger
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -11,14 +12,21 @@ class LogInterceptor : Interceptor {
         val response = chain.proceed(request)
         val buffer = Buffer()
         request.body()?.writeTo(buffer)
-        Logger.v("HTTP Request: ${response.request().url()}," +
-                " method: ${response.request().method()}," +
-                " status: ${response.code()}," +
-                " request body: ${buffer.readUtf8()}")
-        if(!response.isSuccessful) {
-            Logger.e("\n${response.request().url()} Failed with error: ${response.message()}")
+        Logger.v(
+            "HTTP Request: ${response.request().url()}," +
+                    " method: ${response.request().method()}," +
+                    " status: ${response.code()}," +
+                    " request body: ${buffer.readUtf8()}"
+        )
+        if (!response.isSuccessful) {
+            Logger.e(
+                message = "\n${response.request().url()}, method: ${ 
+                    response.request().method()
+                }, Failed with error: ${response.message()}\n",
+                attributes = mapOf("response" to Gson().toJson(response))
+            )
         } else {
-            Logger.v("\nResponse: ${ response.peekBody(2048).string()}")
+            Logger.v("\nResponse: ${response.peekBody(2048).string()}")
         }
         return response
     }
